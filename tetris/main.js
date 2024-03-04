9b913dc70d21734c608c283496cdb4b724b11e31
santicano <canosantiago404@gmail.com>
2024-03-04 20:09:54 -0300

    checkeadas colisiones

--- tetris/main.js
+++ tetris/main.js
@@ -87,10 +87,34 @@ function draw() {
 }
 
 document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") piece.position.x--;
-  if (event.key === "ArrowRight") piece.position.x++;
-  if (event.key === "ArrowDown") piece.position.y++;
-  if (event.key === "ArrowUp") piece.position.y--;
+  if (event.key === "ArrowLeft") {
+    piece.position.x--;
+    if (checkCollision()) {
+      piece.position.x++;
+    }
+  }
+  if (event.key === "ArrowRight") {
+    piece.position.x++;
+    if (checkCollision()) {
+      piece.position.x--;
+    }
+  }
+  if (event.key === "ArrowDown") {
+    piece.position.y++;
+    if (checkCollision()) {
+      piece.position.y--;
+    }
+  }
 });
+
+function checkCollision() {
+  return piece.shape.find((row, y) => {
+    return row.find((value, x) => {
+      return (
+        value !== 0 && board[y + piece.position.y]?.[x + piece.position.x] !== 0
+      );
+    });
+  });
+}
 
 update();
